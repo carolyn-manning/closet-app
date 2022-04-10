c6149b356c47876d9c065313323b0ad71fa5cc68
carolyn-manning <carolyn.manning3@gmail.com>
2022-04-09 22:06:39 -0400

    create frontend and backend. generate item resource in backend

---
+++ backend/db/migrate/20220410015854_create_items.rb
@@ -0,0 +1,11 @@
+class CreateItems < ActiveRecord::Migration[6.1]
+  def change
+    create_table :items do |t|
+      t.string :brand
+      t.string :size
+      t.string :color
+
+      t.timestamps
+    end
+  end
+end
