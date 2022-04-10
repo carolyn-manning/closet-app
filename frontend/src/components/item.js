34b0c75edf498456ca2d2225ac419f97d6648b61
carolyn-manning <carolyn.manning3@gmail.com>
2022-04-09 22:25:00 -0400

    create simple item and items components

---
+++ frontend/src/components/item.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Item extends Component {
+  render () {
+    return (
+      <div className="App">
+
+      </div>
+      );
+    }
+}
+
+export default Item;
---
+++ frontend/src/components/items.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Items extends Component {
+  render () {
+    return (
+      <div className="App">
+
+      </div>
+      );
+    }
+}
+
+export default Items;
