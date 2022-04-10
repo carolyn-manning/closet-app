1e3ed0edab22f67363b19730235db7e5367dcd4b
carolyn-manning <carolyn.manning3@gmail.com>
2022-04-09 22:10:37 -0400

    seed database

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Item.create(brand:"Gucci", size: "1", color:"black")
+Item.create(brand:"Free People", size: "2", color:"blue")
+Item.create(brand:"Zara", size: "3", color:"red")
+Item.create(brand:"Fendi", size: "4", color:"green")
+Item.create(brand:"Prada", size: "5", color:"black")
+Item.create(brand:"Patagonia", size: "6", color:"multi")
