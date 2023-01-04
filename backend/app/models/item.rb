4ca78362233a50ecd17b62c71d64fc6091d0b0af
carolyn-manning <carolyn.manning3@gmail.com>
2023-01-03 20:03:29 -0500

    add scopes to item model on backend in prep for filter component

--- backend/app/models/item.rb
+++ backend/app/models/item.rb
@@ -1,4 +1,10 @@
 class Item < ApplicationRecord
     has_one_attached :image
     belongs_to :user
+
+    scope :color, -> (color) {where(:color => color)}
+    scope :type, -> (type) {where(:type => type)}
+    scope :brand, -> (brand) {where(:brand => brand)}
+    scope :size, -> (size) {where(:size => size)}
+
 end
